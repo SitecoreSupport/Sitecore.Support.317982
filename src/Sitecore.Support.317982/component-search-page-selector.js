af9b3625e5b6f4bc1bbde0319a6a9bbfc34aeb55
iro <iro@sitecore.net>
2019-03-26 13:40:15 +0200

    add missing closing bracket

--- src/Sitecore.Support.317982/component-search-page-selector.js
+++ src/Sitecore.Support.317982/component-search-page-selector.js
@@ -227,6 +227,7 @@
                             if (hash[param] !== "0") {
                                 hashObj[param] = 0;
                                 Backbone.history.navigate(that.createFirstPageUrlHash(hashObj), { trigger: true, replace: true });
+                            }
                         }, 100);
                     } else {
                         newSelectedValue = Math.ceil(data.offset / data.pageSize) + 1;
